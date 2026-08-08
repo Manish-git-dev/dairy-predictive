eb1642d8c5db96b4b085106a1e7d66628d271afb
Manish <manimanish85981@gmail.com>
2026-08-08 13:51:02 +0530

    feat(reports): add report history endpoint

--- server/src/routes/reportRoutes.js
+++ server/src/routes/reportRoutes.js
@@ -5,6 +5,7 @@ const { generateReportSchema } = require('../validators/reportValidator');
 const reportController = require('../controllers/reportController');
 
 router.get('/types', reportController.getReportTypes);
+router.get('/history', authorize('ops_admin', 'manager', 'analyst'), reportController.getHistory);
 router.post('/generate', authorize('ops_admin', 'manager', 'analyst'), validate(generateReportSchema), reportController.generateReport);
 
 module.exports = router;
