37a48c99891dea8c13a4c07bb2959105aee9c6cf
Manish <manimanish85981@gmail.com>
2026-08-08 14:44:25 +0530

    perf: add operational event query indexes

--- server/src/models/OperationalEvent.js
+++ server/src/models/OperationalEvent.js
@@ -14,4 +14,8 @@ const operationalEventSchema = new mongoose.Schema({
   organization: { type: mongoose.Schema.Types.ObjectId, ref: 'Organization', required: true, index: true }
 }, { timestamps: true });
 
+operationalEventSchema.index({ organization: 1, createdAt: -1 });
+operationalEventSchema.index({ organization: 1, eventType: 1, createdAt: -1 });
+operationalEventSchema.index({ organization: 1, 'entity.type': 1, 'entity.id': 1, createdAt: -1 });
+
 module.exports = mongoose.model('OperationalEvent', operationalEventSchema);
