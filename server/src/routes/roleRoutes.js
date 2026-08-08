926bf481a521136bd5bbfbab07a09b68b1d50e13
Manish <manimanish85981@gmail.com>
2026-08-08 14:26:11 +0530

    hardening(role): validate and protect role administration routes

--- server/src/routes/roleRoutes.js
+++ server/src/routes/roleRoutes.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 const { authorize } = require('../middleware/authMiddleware');
+const validate = require('../middleware/validate');
+const { createRoleSchema, updateRoleSchema } = require('../validators/roleValidator');
 const roleController = require('../controllers/roleController');
 
-router.get('/', roleController.getAll);
-router.post('/', authorize('ops_admin'), roleController.create);
-router.get('/:id', roleController.getById);
-router.put('/:id', authorize('ops_admin'), roleController.update);
+router.get('/', authorize('ops_admin', 'manager'), roleController.getAll);
+router.post('/', authorize('ops_admin'), validate(createRoleSchema), roleController.create);
+router.get('/:id', authorize('ops_admin', 'manager'), roleController.getById);
+router.put('/:id', authorize('ops_admin'), validate(updateRoleSchema), roleController.update);
 router.delete('/:id', authorize('ops_admin'), roleController.delete);
 
 module.exports = router;
