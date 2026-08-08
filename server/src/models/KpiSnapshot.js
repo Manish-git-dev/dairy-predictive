41c45dd5192c7f841c1cb7935bcc74a86cbf7c20
Manish <manimanish85981@gmail.com>
2026-08-08 14:43:20 +0530

    perf: add KPI snapshot query indexes

--- server/src/models/KpiSnapshot.js
+++ server/src/models/KpiSnapshot.js
@@ -19,4 +19,7 @@ const kpiSnapshotSchema = new mongoose.Schema({
   organization: { type: mongoose.Schema.Types.ObjectId, ref: 'Organization', required: true, index: true }
 }, { timestamps: true });
 
+kpiSnapshotSchema.index({ organization: 1, date: 1 });
+kpiSnapshotSchema.index({ organization: 1, period: 1, date: 1 });
+
 module.exports = mongoose.model('KpiSnapshot', kpiSnapshotSchema);
