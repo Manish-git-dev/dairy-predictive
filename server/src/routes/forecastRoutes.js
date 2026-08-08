f9365089af41580622985d1bf4fd88b4030789b0
Manish <manimanish85981@gmail.com>
2026-08-08 12:51:35 +0530

    feat(forecast): add unified baseline generation route

--- server/src/routes/forecastRoutes.js
+++ server/src/routes/forecastRoutes.js
@@ -4,6 +4,7 @@ const validate = require('../middleware/validate');
 const { generateForecastSchema } = require('../validators/forecastValidator');
 const forecastController = require('../controllers/forecastController');
 
+router.post('/generate', authorize('ops_admin', 'manager', 'analyst'), validate(generateForecastSchema), forecastController.generateForecast);
 router.post('/demand', authorize('ops_admin', 'manager', 'analyst'), validate(generateForecastSchema), forecastController.generateDemandForecast);
 router.post('/workload', authorize('ops_admin', 'manager', 'analyst'), validate(generateForecastSchema), forecastController.generateWorkloadForecast);
 router.post('/resource', authorize('ops_admin', 'manager', 'analyst'), validate(generateForecastSchema), forecastController.generateResourceForecast);
