0a662dcc047bbd302f24de41890c817aae262c08
Manish <manimanish85981@gmail.com>
2026-08-08 14:37:17 +0530

    security(tenant): validate active organization context

--- server/src/middleware/organizationMiddleware.js
+++ server/src/middleware/organizationMiddleware.js
@@ -1,12 +1,29 @@
 const ApiError = require('../utils/ApiError');
+const Organization = require('../models/Organization');
 
-const organizationMiddleware = (req, res, next) => {
-  if (!req.user || !req.user.organization) {
-    return next(new ApiError(403, 'User does not belong to any organization'));
+const organizationMiddleware = async (req, res, next) => {
+  try {
+    if (!req.user || !req.user.organization) {
+      return next(new ApiError(403, 'User does not belong to any organization'));
+    }
+
+    const organizationId = req.user.organization._id || req.user.organization;
+    const organization = await Organization.findById(organizationId).select('_id isActive');
+
+    if (!organization) {
+      return next(new ApiError(403, 'Organization not found'));
+    }
+
+    if (!organization.isActive) {
+      return next(new ApiError(403, 'Organization is inactive'));
+    }
+
+    req.organizationId = organization._id;
+    req.organization = organization;
+    next();
+  } catch (error) {
+    next(error);
   }
-  
-  req.organizationId = req.user.organization._id || req.user.organization;
-  next();
 };
 
 module.exports = { setOrganization: organizationMiddleware };
